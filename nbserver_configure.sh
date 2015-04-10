3707f49639f52e1cdf2913b7feaf28f8a8c45ed1
daweeed <david.forgeron@gmail.com>
2015-04-10 18:56:53 +0200

    copy notebooks from pypot doc & changing notebook store in daemon

--- nbserver_configure.sh
+++ nbserver_configure.sh
@@ -7,6 +7,11 @@ ipython profile create nbserver
 cp /home/poppy/.ipython/profile_nbserver/ipython_notebook_config.py /home/poppy/.ipython/profile_nbserver/ipython_notebook_config.py.BAK
 cp conf/ipython_notebook_config.py /home/poppy/.ipython/profile_nbserver/ipython_notebook_config.py
 
+# copy notebooks from pypot doc
+mkdir /home/poppy/notebooks
+cp -R /home/poppy/dev/pypot/samples/notebooks/ /home/poppy/notebooks/sample/
+
+
 # ipython notebook --profile=nbserver
 # Deamonize
 sudo cp conf/nbserver /etc/init.d/nbserver
