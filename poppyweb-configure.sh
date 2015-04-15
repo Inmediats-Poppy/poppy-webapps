b9411784cc87936a9c4fc0c9c88a52fc626a3754
daweeed <david.forgeron@gmail.com>
2015-04-15 18:07:52 +0200

    Correcting path and git repo URL

--- poppyweb-configure.sh
+++ poppyweb-configure.sh
@@ -6,14 +6,15 @@
 sudo mv /etc/apache2/sites-enabled/000-default.conf /etc/apache2/sites-enabled/wificonfig.conf
 sudo a2ensite 000-default.conf
 sudo /etc/init.d/apache2 restart
-sudo mv /var/www/web/ /var/www/html/wificonfig/
+sudo mv /var/www/web /var/www/html/wificonfig
  
 # Moving WWW/
-sudo mv www/html/ /var/www/html/
+sudo mv www/html/ /var/www/
 
 #mv snap
-git clone snap.git
-sudo mv snap/ /var/www/html/
+git clone https://github.com/jmoenig/Snap--Build-Your-Own-Blocks.git
+mv Snap--Build-Your-Own-Blocks snap
+sudo mv snap /var/www/html/
 
 # mv snap blocks
 sudo mv conf/pypot-basic-snap-blocks.xml /var/www/html/snap/Examples/
