a27f9d133f06ad90335fb5ca321a6fd31ad19e52
daweeed <david.forgeron@gmail.com>
2015-04-20 19:39:40 +0200

    Debugging poppyAP (Work In Progress)

--- poppyAP_configure.sh
+++ poppyAP_configure.sh
@@ -4,9 +4,9 @@ set -x
 trap read debug
 
 # Creating variable for Wlan inerface
-export WLANINT=$(ifconfig | grep wlan | cut -c -6)
+#export WLANINT=$(ifconfig | grep wlan | cut -c -6)
 # configuring with Wlan variable
-sed -i -e "s/WLANINT/$WLANINT/g" *
+#sed -i -e "s/WLANINT/$WLANINT/g" *
 
 
 # installating software
@@ -18,16 +18,17 @@ sudo cp /etc/network/interfaces /etc/network/interfaces.BAK
 sudo mv conf/interfaces /etc/network/interfaces 
 
 # Changing NetworkManager configuration
-sudo cp /etc/NetworManager/NetworManager.conf /etc/NetworManager/NetworManager.conf.BAK
-sudo mv conf/iNetworManager.conf /etc/NetworManager/NetworManager.conf
+sudo cp /etc/NetworkManager/NetworkManager.conf /etc/NetworManager/NetworManager.conf.BAK
+sudo mv conf/iNetworkManager.conf /etc/NetworkManager/NetworManager.conf
 sudo nmcli nm wifi off
 sudo rfkill unblock wlan
-sudo ifdown ${WLANINT}
-sudo ifup ${WLANINT}
+sudo ifdown wlan2 #${WLANINT}
+sudo ifup wlan2 #${WLANINT}
 
 #configure hostapd
 sudo /etc/hostapd/hostapd.conf /etc/hostapd/hostapd.conf.BAK
-sudo mv conf/ihostpad.conf /etc/hostapd/hostapd.conf
+sudo mv conf/hostpad.conf /etc/hostapd/hostapd.conf
+#modify /etc/default/hostapd DAEMON_CONF="/etc/hostapd/hostapd.conf"
 
 # adafruit hack to make driver rtl871xdrv working with hostapd
 wget http://adafruit-download.s3.amazonaws.com/adafruit_hostapd_14128.zip
